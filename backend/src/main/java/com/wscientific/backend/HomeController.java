3025bb773f6c69c030512e49940eeb42bd98bc7f
CHAKFI <ahmedchakfi1999@gmail.com>
2021-05-06 01:37:51 +0000

    first upd "Hello"

---
+++ backend/src/main/java/com/wscientific/backend/HomeController.java
@@ -0,0 +1,17 @@
+package com.wscientific.backend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String Home(){
+      return "Hello from SpringBoot CASABLANCA";
+    }
+
+}
