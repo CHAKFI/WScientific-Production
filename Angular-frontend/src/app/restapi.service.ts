717d13a69521f916437283111111dad45276b367
CHAKFI <ahmedchakfi1999@gmail.com>
2021-06-04 13:45:56 +0000

    Create restApi service

---
+++ Angular-frontend/src/app/restapi.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestapiService {
+
+  constructor(private http:HttpClient) { }
+
+
+  public login(username:string, password:string){
+    const headers = new HttpHeaders({Authorization: 'Basic '+btoa(username+":"+password)})
+    this.http.get("http://localhost:8085/", {headers, responseType:'text' as 'json'});
+  }  
+
+  public getUsers(){
+    let username = "CHAKFI";
+    let password = "polysb";
+    const headers = new HttpHeaders({Authorization: 'Basic '+btoa("CHAKFI"+":"+"polysb")})
+    this.http.get("http://localhost:8085/getUsers", {headers});
+  }  
+
+}
